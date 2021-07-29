e5adc945c04e63a5c0db1cdb5dca6f6f18e31c17
Jose Bustillo <jabustillo@uninorte.edu.co>
2021-07-29 14:27:36 -0500

    feat: add Header component

---
+++ components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { primary } from '../constants/colors';
+
+const Header = ({ title }) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 90,
+    padding: 36,
+    backgroundColor: primary,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headerTitle: {
+    color: 'white',
+    fontSize: 18
+  }
+});
+ 
+export default Header;
