b665b8e1f526ffe8b5e34dc1e3fd1dbba6a2c656
Jose Bustillo <jabustillo@uninorte.edu.co>
2021-07-30 14:18:01 -0500

    feat: add GameOvercomponent and gameOverhandler, and add restartGameHandler

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
+import GameOverScreen from './screens/GameOverScreen';
 import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
+    setGuessRounds(0);
   };
 
-  let content = !userNumber ? <StartGameScreen onStartGame={startGameHandler} /> : <GameScreen />;
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
+  };
+
+  const restartGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber && guessRounds < 1) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
+  } else if (guessRounds > 0) {
+    content = <GameOverScreen rounds={guessRounds} userNumber={userNumber} onRestart={restartGameHandler} />;
+  }
+
 
   return (
     <View style={styles.container}>
