363c5f6e7822e5bb960f025b9322914ecc70d165
Jose Bustillo <jabustillo@uninorte.edu.co>
2021-07-30 07:57:57 -0500

    feat: add function to manage the state of the game

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
+import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = !userNumber ? <StartGameScreen onStartGame={startGameHandler} /> : <GameScreen />;
+
   return (
     <View style={styles.container}>
       <Header title="Guess a number" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -14,7 +14,7 @@ import NumberContainer from '../components/NumberContainer';
 
 import { primary, secondary } from '../constants/colors';
 
-const StartGameScreen = () => {
+const StartGameScreen = ({ onStartGame }) => {
   const [enteredValue, setEnteredValue] = useState('');
   const [confirmed, setConfirmed] = useState(false);
   const [selectedNumber, setSelectedNumber] = useState();
@@ -51,7 +51,7 @@ const StartGameScreen = () => {
       <Card style={styles.summaryContainer}>
         <Text>Chosen Number: </Text>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button title="START GAME" />
+        <Button title="START GAME" onPress={() => onStartGame(true)} />
       </Card>
     );
   }
