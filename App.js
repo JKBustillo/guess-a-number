10663174b37685b93c191807179e02c7b2b8c479
Jose Bustillo <jabustillo@uninorte.edu.co>
2021-08-01 20:19:48 -0500

    feat: load fonts at the start of the app

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
 import Header from './components/Header';
 import GameOverScreen from './screens/GameOverScreen';
 import GameScreen from './screens/GameScreen';
@@ -8,6 +11,19 @@ import StartGameScreen from './screens/StartGameScreen';
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  void (async () => {
+    await Font.loadAsync({
+      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    });
+    setDataLoaded(true);
+  })();
+
+  if (!dataLoaded) {
+    return <AppLoading />;
+  }
 
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
